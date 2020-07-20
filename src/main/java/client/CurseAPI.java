dbb98d9eff3f676250d60f9139d80e0e4df613b1
ozzyian <ogustafsson1@gmail.com>
2020-07-21 00:11:34 +0200

    Fix/changed import order

--- src/main/java/client/CurseAPI.java
+++ src/main/java/client/CurseAPI.java
@@ -1,14 +1,14 @@
 package client;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Handles the api requests for cure addon data.
