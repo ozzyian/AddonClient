58f58c02b1c2221c51c77f93d2e85be7bc2a2086
Rebecka Skareng <ogustafsson1@gmail.com>
2020-07-29 16:46:32 +0200

    Added method to compare versions.

---
+++ src/main/java/client/AddonUpdater.java
@@ -0,0 +1,47 @@
+package client;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+/**
+ * Handles the updating of addons.
+ */
+public class AddonUpdater {
+
+    private CurseAPI curseAPI;
+
+    public AddonUpdater(CurseAPI curseAPI) {
+        this.curseAPI = curseAPI;
+    }
+
+    /**
+     *
+     * @param check
+     * @return
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public boolean isUpToDate(Addon check) throws IOException, InterruptedException {
+
+        LocalDateTime currentFileDate = check.getLatestFileDate();
+        LocalDateTime otherFileDate = curseAPI.getAddon(check.getId() + "").getLatestFileDate();
+
+        return currentFileDate.isEqual(otherFileDate);
+    }
+    
+    /**
+     * Updates a specific addon object with new data.
+     * 
+     * @param toUpdate The addon that should be updated
+     * @return Addon with the new data.
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    public Addon updateAddon(Addon toUpdate) throws IOException, InterruptedException {
+
+        Addon updated = curseAPI.getAddon(toUpdate.getId()+"");
+
+        return updated;
+    }
+
+}
